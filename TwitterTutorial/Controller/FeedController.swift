490fbdd5128aada7f19d18e4363f81a5007d4252
playkang <scka@naver.com>
2021-10-14 14:43:06 +0900

    temp

--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -10,6 +10,10 @@ import UIKit
 class FeedController: UIViewController {
     // MARK: - Properties
     
+    var user: User? {
+        didSet { print("DEBUG : Did set user in feed controller..") }
+    }
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -25,5 +29,12 @@ class FeedController: UIViewController {
         let imageView = UIImageView(image: UIImage(named: "twitter_logo_blue"))
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
+        
+        let profileImageView = UIImageView()
+        profileImageView.backgroundColor = .twitterBlue
+        profileImageView.setDimensions(width: 32, height: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
     }
 }
