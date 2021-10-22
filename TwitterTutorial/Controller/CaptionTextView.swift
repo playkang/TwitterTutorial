dc734c82e2f593c3576c35b9435e019ccc544563
playkang <scka@naver.com>
2021-10-22 18:49:38 +0900

    .

---
+++ TwitterTutorial/Controller/CaptionTextView.swift
@@ -0,0 +1,24 @@
+//
+//  CaptionTextView.swift
+//  TwitterTutorial
+//
+//  Created by Kang on 2021/10/22.
+//
+
+import UIKit
+
+class CaptionTextView: UITextView {
+    
+    // MARK: - properties
+    
+    let placeholderlabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    
+}
