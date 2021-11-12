22a06660ea98b3ea3b96992c4acd5a898422f776
playkang <scka@naver.com>
2021-11-13 04:42:00 +0900

    ..

--- TwitterTutorial/Controller/UploadTweetController.swift
+++ TwitterTutorial/Controller/UploadTweetController.swift
@@ -23,7 +23,7 @@ class UploadTweetController: UIViewController {
         button.frame = CGRect(x: 0, y: 0, width: 64, height: 32)
         button.layer.cornerRadius = 32 / 2
         
-//        button.addTarget(self, action: #selector(handleUploadTweet), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handleUploadTweet), for: .touchUpInside)
         
         return button
     }()
